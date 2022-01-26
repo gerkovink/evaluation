d6990cdb1ea7f4843d9644f94053e496d1e7355c
hanneoberman <h.i.oberman@uu.nl>
2022-01-26 17:31:27 +0100

    Split up simulation function into parts

--- R/run_sim.R
+++ R/run_sim.R
@@ -9,7 +9,6 @@ source("R/utils.R")
 n_sim <- 1000 #c(100, 1000)
 n_obs <- c(50, 500, 5000)
 n_var <- 2 #c(2, 5, 20)
-means <- c(10, 100)
 corr <- c(0, .8)
 mis_mech <- c("MCAR", "MAR", "MNAR")
 mis_type <- "RIGHT" #c("left", "right", "mid", "tail")
@@ -27,6 +26,58 @@ est <- c("mean(Y)", "var(Y)", "cov(X,Y)", "lm(Y~X)", "lm(X~Y)")
 # diag(varcov) <- 1
 # varcov[1, 2] <- varcov[2, 1] <- corr 
 
+# generate complete data
+generation <- function(...){
+  dat <- data.frame(mvtnorm::rmvnorm(
+    n = n_obs,
+    mean = c(10, 100),
+    sigma = matrix(c(1, corr, corr, 1), 2, 2)
+  )) %>% setNames(c("Y", "X"))
+}
+
+# ampute the data
+amputation <- function(dat, ...){
+  amp <- dat %>% 
+  mice::ampute(
+    prop = mis_prop,
+    mech = mis_mech,
+    type = mis_type
+  ) %>% 
+  .$amp}
+
+# impute the data
+imputation <- function(amp, ...){
+  imp <- amp %>%
+  mice::mice(
+    m = n_imp,
+    method = imp_meth,
+    maxit = n_it,
+    print = FALSE
+  )}
+
+# evaluate the imputations
+evaluation <- function(imp, dat, ...){
+  fit <- imp %>% 
+    with(lm(Y ~ X))
+  
+  rmses <- purrr::map_dfr(1:imp$m, function(.m){
+    data.frame(
+      rmse_cell = rmse(imp$imp$Y[[.m]] - dat$Y[imp$where[, "Y"]]), 
+      rmse_pred = rmse(fit$analyses[[.m]]$residuals))    
+  }) %>% colMeans() %>% t()
+  
+  fit %>% 
+    mice::pool() %>% 
+    broom::tidy(conf.int = TRUE) %>% 
+    .[2, ] %>% 
+    mutate(
+      bias = estimate - corr,
+      ciw = conf.high - conf.low,
+      cr = conf.low <= corr && corr <= conf.high,
+      .keep = "none") %>% 
+    cbind(., rmses)
+}
+
 # simulation function
 run <- function(
   n_obs = 500,
@@ -34,80 +85,34 @@ run <- function(
   mis_mech = "MAR",
   mis_type = "RIGHT",
   mis_prop = 0.5,
-  n_imp = 5,
-  n_it = 10,
   imp_meth = "norm",
-  ...){
+  n_imp = 5,
+  n_it = 10){
     
-    # generate data
-    dat <- purrr::map(n_obs,
-      ~{data.frame(mvtnorm::rmvnorm(
-      n = .x,
-      mean = means,
-      sigma = matrix(c(1, corr, corr, 1), 2, 2)
-    )) %>% setNames(c("Y", "X"))}) %>% 
-      setNames(paste0("n_obs_", n_obs))
+  # generate data
+  dat <- generation(n_obs, corr)
+  
+  # ampute the data
+  amp <- amputation(dat, mis_mech, mis_prop, mis_type)
     
-    # ampute
-    amp <- purrr::map(
-      dat,
-      function(.d){
-        mice::ampute(.d,
-        prop = mis_prop,
-        mech = mis_mech,
-        type = mis_type
-        ) %>% 
-      .$amp}) 
-    
-    # impute and fit analysis model
-    imp <- purrr::map(
-      amp,
-      function(.a){
-        mice::mice(.a,
-        m = n_imp,
-        method = imp_meth,
-        maxit = n_it,
-        print = FALSE
-      ) %>%
-      with(lm(Y ~ X))})
-
-    # pool results
-    res <- purrr::map(
-      imp,
-      function(.i){
-        mice::pool(.i) %>% 
-        broom::tidy(conf.int = TRUE) %>% 
-        .[2, ] %>% 
-      mutate(bias = estimate - corr,
-             ciw = conf.high - conf.low,
-             cr = conf.low <= corr && corr <= conf.high,
-             .keep = "none") %>% 
-      cbind(rmse_pred = purrr::map_dbl(
-        1:n_imp, 
-        function(.m){
-          rmse(.i$analyses[[.m]]$residuals)}) %>% 
-          mean())
-      })
-    # out <- 
-    #   ~{   %>% 
-    #   cbind(
-    #     rmse_pred = purrr::map_dbl(1:n_imp, 
-    #     ~{rmse(imp[[.]]$analyses[[.x]]$residuals)}) %>% 
-    #       mean(),
-    #     rmse_cell = purrr::map_dbl(1:n_imp, 
-    #       ~{rmse(imp$analyses[[.x]]$model$Y[is.na(amp$Y)], dat$Y[is.na(amp$Y)])}) %>% 
-    #       mean()
-    #     )})
-    
-    # output
-    return(res)
-  }
+  # impute the data
+  imp <- imputation(amp, imp_meth, n_imp, n_it)
+  
+  # evaluate the imputations 
+  out <- evaluation(imp, dat)
+  
+  # output
+  return(out)
+}
 
 # test once
 run()
 
 # test with multiple conditions
-a <- purrr::map_dfr(n_obs, ~{run(.x) %>% cbind(n_obs = .x, .)})
+purrr::map_dfr(c(50, 500), ~{run(n_obs = .x) %>% cbind(n_obs = .x, .)})
+purrr::map_dfr(c(0, .4, .8), ~{run(corr = .x) %>% cbind(corr = .x, .)})
 
-# test simulation
-b <- replicate(n = 2, purrr::map_dfr(n_obs, ~{run(.x) %>% cbind(n_obs = .x, .)}), simplify = FALSE)
+# test with more simulation repetitions
+sims <- purrr::map_dfr(1:10, ~{
+  purrr::map_dfr(c(0, .4, .8), function(.c){run(corr = .c) %>% cbind(it = .x, corr = .c, .)})})
+sims %>% group_by(corr) %>% summarise(mean(bias))
