cdf8324b5869c9e9f0737de1ebf09e47d3e2e6aa
hanneoberman <h.i.oberman@uu.nl>
2022-03-07 12:47:54 +0100

    Version to compare with latex diff or pdf diff

--- R/run_sim.R
+++ R/run_sim.R
@@ -1,9 +1,9 @@
 # simulation study for illustration
 
 # set-up 
-# set.seed(123)
-# library(dplyr)
-# source("R/utils.R")
+set.seed(123)
+library(dplyr)
+source("R/utils.R")
 n_sim = 2
 
 ## simulation parameters ##
@@ -85,14 +85,14 @@ evaluation <- function(imp, amp, dat, r){
   # performance
   out <- out %>% 
     cbind(
-      # n_obs = nrow(dat), 
-      # corr = r, 
-      # mis_mech = amp$mech,
-      # mis_type = amp$type,
-      # mis_prop = amp$prop,
-      # imp_meth = imp$method[1],
-      # n_imp = imp$m,
-      # n_it = imp$iteration,
+      n_obs = nrow(dat),
+      corr = r,
+      mis_mech = amp$mech,
+      mis_type = amp$type,
+      mis_prop = amp$prop,
+      imp_meth = imp$method[1],
+      n_imp = imp$m,
+      n_it = imp$iteration,
       .,
       ave = c(mean(dat$Y), mean(amp$amp$Y, na.rm = TRUE), mean(mice::complete(imp, "long")$Y)),
       rmses,
@@ -132,120 +132,3 @@ return(out)
 results <- purrr::map_dfr(1:n_sim, ~{
   cbind(sim = .x, simulation())
 })
-
-# all_par <- expand.grid(
-#   n_obs = n_obs, 
-#   corr = corr,
-#   mis_mech = mis_mech,
-#   mis_type = mis_type,
-#   mis_prop = mis_prop,
-#   imp_meth = imp_meth,
-#   n_imp = n_imp,
-#   n_it = n_it)
-
-# # simulate complete datasets
-# dat <- purrr::map(n_obs, function(n){
-#   purrr::map(corr, function(r){
-#     generation(n, r)
-#   }) %>% setNames(corr)
-# }) %>% setNames(n_obs)
-# 
-# # ampute all datasets
-# amp <- purrr::map(n_obs, function(n){
-#   purrr::map(corr, function(r){
-#     purrr::map(mis_mech, function(mech){
-#   amputation(dat[[n]][[r]], mech, type = mis_type, prop = mis_prop)})})})
-
-# impute all incomplete datasets
-
-
-
-# l2 <- list(
-#   list(num = 1:3,     letters[1:3]),
-#   list(num = 101:103, letters[4:6]),
-#   list()
-# )
-# l2 %>% purrr::map(c(2, 2))
-# 
-# 
-# par_dat <- list(
-#   n_obs = c(50, 500, 5000),
-#   corr = c(0, .4, .8))
-# 
-# par_dat %>% purrr::map(~generation(n = n_obs, r = corr))
-
-# expand.grid(height = seq(60, 80, 5), weight = seq(100, 300, 50),
-#             sex = c("Male","Female"))
-
-# # simulation function
-# run <- function(
-#   n_obs = 500,
-#   corr = 0.4,
-#   mis_mech = "MAR",
-#   mis_type = "RIGHT",
-#   mis_prop = 0.5,
-#   imp_meth = "norm",
-#   n_imp = 5,
-#   n_it = 10){
-#     
-#   # generate data
-#   dat <- generation(n = n_obs, r = corr)
-#   
-#   # ampute the data
-#   amp <- amputation(dat, mech = mis_mech, type = mis_type, prop = mis_prop)
-#     
-#   # impute the data
-#   imp <- imputation(amp, meth = imp_meth, m = n_imp, it = n_it)
-#   
-#   # evaluate the imputations 
-#   out <- evaluation(imp, amp, dat, r = corr)
-#   
-#   # output
-#   return(out)
-# }
-
-# # test once
-# run()
-# run(corr = 0.8)
-# 
-# # test with multiple conditions
-# purrr::map_dfr(c(50, 500), ~{run(n_obs = .x)}) 
-# purrr::map_dfr(c(0, .4, .8), ~{run(corr = .x)}) 
-# purrr::map_dfr(c("MCAR", "MAR", "MNAR"), ~{run(mis_mech = .x)}) 
-# 
-# # test with more simulation repetitions
-# sims <- purrr::map_dfr(1:5, ~{
-#   purrr::map_dfr(c(0, .4, .8), function(.c){run(corr = .c) %>% cbind(sim = .x, .)})})
-# sims %>% group_by(corr) %>% summarise(mean(rmse_pred))
-
-# # parameters
-# n_sim <- 1000 #c(100, 1000)
-# n_obs <- 500 #c(50, 500, 5000)
-# n_var <- 2 #c(2, 5, 20)
-# corr <- 0.4 #c(0, .8)
-# mis_mech <- "MAR" #c("MCAR", "MAR", "MNAR")
-# mis_type <- "RIGHT" #c("left", "right", "mid", "tail")
-# mis_prop <- 0.5 #c(.1, .25, .5)
-# n_imp <- 5 #c(1, 5)
-# imp_meth <- "norm" #c("mean", "norm.predict", "norm") #cca
-# n_it <- 10 #c(1, 5, 10)
-# # est <- c("mean(Y)", "var(Y)", "cov(X,Y)", "lm(Y~X)", "lm(X~Y)")
-# # model vs design based
-# # with or without sampling variance
-
-# refs <- data.frame(
-#   mean_full = mean(dat$Y),
-#   mean_cca = mean(amp$amp$Y, na.rm = TRUE),
-#   mean_est = mean(mice::complete(imp, "long")$Y),
-#   beta_full = coef(lm(Y~X, dat))[2],
-#   beta_cca = coef(lm(Y~X, amp$amp, na.action = na.omit))[2])
-
-# <- fit %>% 
-#   mice::pool() %>% 
-#   broom::tidy(conf.int = TRUE) %>% 
-#   .[2, ] %>% 
-#   mutate(
-#     beta_est = estimate,
-#     beta_ciw = conf.high - conf.low,
-#     beta_cr = conf.low <= r && r <= conf.high,
-#     .keep = "none") %>% 
